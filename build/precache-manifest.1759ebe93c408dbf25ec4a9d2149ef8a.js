a18ef186613b400d40d1fade5e79cbe2c0f8ae9b
Leslie <leslie2515lopez@gmail.com>
2021-06-04 19:21:08 -0500

    adding files for deploying

---
+++ build/precache-manifest.1759ebe93c408dbf25ec4a9d2149ef8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2ec6baddef3b5fe58233fab65c16d4",
+    "url": "/react-travel-website/index.html"
+  },
+  {
+    "revision": "e21d88e9f2bf4e5cfe20",
+    "url": "/react-travel-website/static/css/main.b3209779.chunk.css"
+  },
+  {
+    "revision": "f8ade70d03d49f25568e",
+    "url": "/react-travel-website/static/js/2.4c7eac03.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-travel-website/static/js/2.4c7eac03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21d88e9f2bf4e5cfe20",
+    "url": "/react-travel-website/static/js/main.c8a76af8.chunk.js"
+  },
+  {
+    "revision": "8678d6c9e44d158288e8",
+    "url": "/react-travel-website/static/js/runtime-main.2acd0b6e.js"
+  }
+]);
