21d7ea843dabeaa844426c4256ee529ac3c3265a
Leslie Lopez <leslie2515lopez@gmail.com>
2021-09-23 19:06:06 -0500

    Added build

---
+++ build/precache-manifest.3cd3791ac03e7493c140b1f98d9b40ec.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbe7692fe1dcdd01dc727d80d624195",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df276932509feb931590",
+    "url": "./static/css/main.54b4bd3a.chunk.css"
+  },
+  {
+    "revision": "280af79c3ff8ab9bd59a",
+    "url": "./static/js/2.7445b8cb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.7445b8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df276932509feb931590",
+    "url": "./static/js/main.505567c2.chunk.js"
+  },
+  {
+    "revision": "c18084ba232b034402d6",
+    "url": "./static/js/runtime-main.28314ff7.js"
+  },
+  {
+    "revision": "5b1f04e4ee82e4a017c44b91140a2760",
+    "url": "./static/media/img-1.5b1f04e4.jpg"
+  },
+  {
+    "revision": "ae7b8282288349f1e2a86f911a3edba0",
+    "url": "./static/media/img-2.ae7b8282.jpg"
+  },
+  {
+    "revision": "c6a22969b942f85f1c0dce8139dd6a57",
+    "url": "./static/media/img-3.c6a22969.jpg"
+  },
+  {
+    "revision": "1f157bf6d14c3df040228d01045fac3a",
+    "url": "./static/media/img-4.1f157bf6.jpg"
+  },
+  {
+    "revision": "7c9abd49a8b7b8b8ab06de6fddf0322b",
+    "url": "./static/media/img-5.7c9abd49.jpg"
+  },
+  {
+    "revision": "3c73c994c493bac85b103c49e27019d1",
+    "url": "./static/media/img-6.3c73c994.jpg"
+  },
+  {
+    "revision": "67b1d79de7eda58bb992918ef3f6e5ee",
+    "url": "./static/media/img-7.67b1d79d.jpg"
+  },
+  {
+    "revision": "8a663070e184b61171f9f567de024ca3",
+    "url": "./static/media/img-8.8a663070.jpg"
+  },
+  {
+    "revision": "6709b3d096306953953b36629804d391",
+    "url": "./static/media/video-1.6709b3d0.mp4"
+  }
+]);
